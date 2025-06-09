2cf9c59451be09d2628341ccc5a365c26ed92a49
Bene SAYER <bentouster@gmail.com>
2025-06-09 19:40:12 +0200

    Repositories added for Program and Category

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS program;
+DROP TABLE IF EXISTS category;
+
 create table user (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
@@ -12,14 +17,14 @@ create table item (
 );
 
 insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
-
+values 
+(1, "jdoe@mail.com", "123456");
 insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
-
+values 
+(1, "Stuff", 1),
+(2, "Doodads", 1);
+  
+DROP TABLE IF EXISTS program;
 CREATE TABLE program(
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   title VARCHAR(255) NOT NULL,
@@ -28,40 +33,38 @@ CREATE TABLE program(
   country VARCHAR(255) NOT NULL,
   year INT NOT NULL,
   category_id INT
-);  
-
+);
+DROP TABLE IF EXISTS category;
 CREATE TABLE category(
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL
 );
 
 INSERT INTO category(id, name)
-VALUES
-(1, "Comédie")
-(2,"Science-fiction");
-
-INSERT INTO program
-(
-  id,
-  title,
-  synopsis,
-  poster,
-  country,
-  year,
-  category_id
-);
-VALUES
-(
-  1,
-  "The Good Place",
+VALUES 
+(1, "Comédie"),
+(2, "Science-fiction");
+  
+INSERT INTO program (
+    id,
+    title,
+    synopsis,
+    poster,
+    country,
+    year,
+    category_id
+  )
+VALUES (
+    1,
+    "The Good Place",
     "À sa mort, Eleanor Shellstrop est envoyée au Bon Endroit, un paradis fantaisiste réservé aux individus exceptionnellement bienveillants. Or Eleanor n'est pas exactement une « bonne personne » et comprend vite qu'il y a eu erreur sur la personne. Avec l'aide de Chidi, sa prétendue âme sœur dans l'au-delà, la jeune femme est bien décidée à se redécouvrir.",
     "https://img.betaseries.com/JwRqyGD3f9KvO_OlfIXHZUA3Ypw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2F94857341d71c795c69b9e5b23c4bf3e7.jpg",
     "USA",
     2016,
     1
-),
-(
-  2,
+  ),
+  (
+    2,
     "Dark",
     "Quatre familles affolées par la disparition d'un enfant cherchent des réponses et tombent sur un mystère impliquant trois générations qui finit de les déstabiliser.",
     "https://img.betaseries.com/zDxfeFudy3HWjxa6J8QIED9iaVw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2Fc47135385da176a87d0dd9177c5f6a41.jpg",
